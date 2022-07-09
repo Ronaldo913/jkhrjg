c56a72fb5a8be2c6db00d6d9eebbc2ebfc165560
Ronaldo Noberto <rns6@aluno.ifal.edu.br>
2022-07-09 20:53:56 -0300

    pronto

---
+++ projeto2/exemplo03/index.ts
@@ -0,0 +1,15 @@
+import Gato from "./Gato"
+
+const gato = new Gato(
+    4,
+    20, 
+    'gato', 
+    '29/11/2004', 
+    'branco e preto',
+    'aaaaaaaaaaaaaaaaaaah'
+)
+
+gato.correr()
+gato.grunhir()
+gato.arranharDono()
+gato.beberLeite()
