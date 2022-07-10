d5be2bc57b07eeefefc5a766b1b3b19c4568939b
Ronaldo Noberto <rns6@aluno.ifal.edu.br>
2022-07-10 01:17:01 -0300

    react coisa linda

--- reactjs/meu-projeto2/src/ui/pages/HomePage.tsx
+++ reactjs/meu-projeto2/src/ui/pages/HomePage.tsx
@@ -1,6 +1,34 @@
+import {useState} from 'react'
+import '../styles/HomePage.css'
+
 function HomePage(){
+    let [valor, setValor] = useState(0)
+
+
     return (
-        <p>Home Page</p>
+        <div id="home-page"> 
+            <h1>{valor}</h1>
+            <p>Valor do computador</p>
+
+            <div id="row-button">
+                <button                      className="action-btn decrease"
+                onClick={ () => setValor(valor--)
+                }
+                >
+                    Diminuir
+                </button>
+                {/* minuto: Ismael->1:01:27  */}
+                <button className="action-btn increase"
+                onClick={
+                    function(){
+                        setValor(valor++)
+                    }
+                }
+                >
+                    Aumentar
+                </button>
+            </div>
+        </div>
     )
 }
 
